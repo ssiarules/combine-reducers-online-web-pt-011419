367d62b91f48e6d12ef2666e78e7d8d7d457b9ad
Johnson Joseph <jhonsonj88@gmail.com>
2019-11-06 17:38:00 +0000

    Automatically backed up by Learn

--- src/reducers/manageAuthorsAndBooks.js
+++ src/reducers/manageAuthorsAndBooks.js
@@ -8,40 +8,40 @@ const rootReducer = combineReducers({
 
 export default rootReducer
 
-export default function bookApp(
-  state = {
-    authors: [],
-    books: []
-  },
-  action
-) {
+
+  function booksReducer(state = [], action){
+  
   let idx;
   switch (action.type) {
     case "ADD_BOOK":
       return {
         ...state,
         books: [...state.books, action.book]
-      };
 
     case "REMOVE_BOOK":
       idx = state.books.findIndex(book => book.id === action.id);
       return {
         ...state,
         books: [...state.books.slice(0, idx), ...state.books.slice(idx + 1)]
+
+        default:
+          return state;
       };
+  }
+
+function authorsReducer(state = [], action){
+  
 
     case "ADD_AUTHOR":
       return {
         ...state,
         authors: [...state.authors, action.author]
-      };
 
     case "REMOVE_AUTHOR":
       idx = state.authors.findIndex(author => author.id === action.id);
       return {
         ...state,
-        authors: [...state.authors.slice(0, idx), ...state.authors.slice(idx + 1)]
-      };
+        authors: [...state.authors.slice(0, idx), ...state.authors.slice(idx + 1)];
 
     default:
       return state;
